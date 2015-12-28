286bdc7e2eabe274e92a2acbfdac6a8cec155e70
tongsh6 <tongsh6@qq.com>
2015-12-28 22:48:42 +0800

    add some file

--- src/main/java/com/loongSmart4j/chapter/HelloServlet.java
+++ src/main/java/com/loongSmart4j/chapter/HelloServlet.java
@@ -1,6 +1,7 @@
 package com.loongSmart4j.chapter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,10 +13,10 @@ import java.util.Date;
 /**
  * Created by Loong on 2015/12/28.
  */
+@WebServlet("/hello")
 public class HelloServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         req.setAttribute("nowTime",dateFormat.format(new Date()));
         req.getRequestDispatcher("/jsp/hello.jsp").forward(req,resp);
